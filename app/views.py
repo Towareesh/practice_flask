5619771b585f0f74d6817df7c4aaa409064b285a
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-01-25 22:28:15 +0200

    login form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 import datetime
 
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/test')
@@ -19,6 +21,7 @@ def test(data=None):
 @app.route('/index/')
 @app.route('/index')
 def index():
+    user = {'username': 'Towareesh'}
     posts = [{'author': {'username': 'Vlad'},
               'body': 'Beautiful day in Portland!'},
 
@@ -28,4 +31,12 @@ def index():
              {'author': {'username': 'Mark'},
               'body': 'Bullshit!'}]
 
-    return render_template('index.html', title='INDEX', posts=posts)
+    return render_template('index.html', title='INDEX', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Войти', form=form)
