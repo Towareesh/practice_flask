c0a022906b616965c70f502f25faf651042833cc
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-01-26 22:19:25 +0200

    feat RegistrationForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,22 @@ class LoginForm(FlaskForm):
     password    = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit      = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    # username   = LoginForm.username
+    # password   = LoginForm.password
+    username   = StringField('Имя', validators=[DataRequired()])
+    password   = PasswordField('Пароль', validators=[DataRequired()])
+    password2  = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    email      = StringField('Почта', validators=[DataRequired(), Email()])
+    submit     = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другое имя.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другую почту.')
