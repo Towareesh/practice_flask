88f7b7db6393df862bb9ee36b8a2c1e69906a8d9
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-01-26 22:18:06 +0200

    fead init db/migrations aid login

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 import os, config
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 # creating an application instance
 app = Flask(__name__)
 app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopmentConfig')
+db = SQLAlchemy(app)
+Migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-from app import views
+from app import views, models
