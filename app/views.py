9ec0323579795a44fbc3c7d6572e252f3ac2ecdb
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-01-28 17:52:55 +0200

    feat

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from werkzeug.urls import url_parse
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm
 from app.models import User
 
 
@@ -17,9 +18,6 @@ def user_del_db(user):
     db.session.delete(user)
     db.session.commit()
 
-def get_user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return user
 
 @app.route('/test')
 @app.route('/test/<data>')
@@ -32,12 +30,12 @@ def test(data=None):
     
     return result
 
+
 @app.route('/')
 @app.route('/index/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Towareesh'}
     posts = [{'author': {'username': 'Vlad'},
               'body': 'Beautiful day in Portland!'},
 
@@ -47,7 +45,8 @@ def index():
              {'author': {'username': 'Mark'},
               'body': 'Bullshit!'}]
 
-    return render_template('index.html', title='INDEX', user=user, posts=posts)
+    return render_template('index.html', title='INDEX', posts=posts)
+
 
 @app.route('/home')
 @login_required
@@ -59,6 +58,7 @@ def home():
     """
 
     return render_template('base.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -86,6 +86,7 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     now_time = datetime.datetime.now()
@@ -105,6 +106,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -114,9 +116,25 @@ def logout():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user = get_user(username)
+    user = User.query.filter_by(username=username).first_or_404()
     posts = [
             {'author': user, 'body': 'Test post_1'},
             {'author': user, 'body': 'Test post_2'}
             ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Изменения сохранены')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
