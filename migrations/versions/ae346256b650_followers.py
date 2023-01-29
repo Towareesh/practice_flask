78cc3d021ebeec462bba2f757c1f55df682284a9
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-01-29 19:23:18 +0200

    upgrade

--- migrations/versions/ae346256b650_followers.py
+++ migrations/versions/ae346256b650_followers.py
@@ -1,8 +1,8 @@
-"""'init_followers_table'
+"""followers
 
-Revision ID: c6dbfcd214ad
-Revises: 5a42ac7c2a79
-Create Date: 2023-01-28 22:07:30.395354
+Revision ID: ae346256b650
+Revises: 37f06a334dbf
+Create Date: 2017-09-17 15:41:30.211082
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c6dbfcd214ad'
-down_revision = '5a42ac7c2a79'
+revision = 'ae346256b650'
+down_revision = '37f06a334dbf'
 branch_labels = None
 depends_on = None
 
