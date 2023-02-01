49d69a46b7175f4b48ad2b34b18d293d45028e02
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-02-01 22:09:32 +0200

    feat follow/unfollow routes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
@@ -6,7 +6,7 @@ from werkzeug.urls import url_parse
 
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from app.forms import EditProfileForm
+from app.forms import EditProfileForm, EmptyForm
 from app.models import User
 
 
@@ -19,10 +19,17 @@ def user_del_db(user):
     db.session.commit()
 
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/test')
 @app.route('/test/<data>')
 def test(data=None):
-    server_gmt_time  = datetime.datetime.now()
+    server_gmt_time  = datetime.now()
     result = server_gmt_time.strftime("%d-%m-%Y %H:%M")
     
     if data is not None:
@@ -89,7 +96,7 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    now_time = datetime.datetime.now()
+    now_time = datetime.now()
     if current_user.is_authenticated:
         return redirect(url_for('index'))
 
@@ -121,7 +128,8 @@ def user(username):
             {'author': user, 'body': 'Test post_1'},
             {'author': user, 'body': 'Test post_2'}
             ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -138,3 +146,43 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash(f'User {username} not found')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash(f'User {username} not found')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
