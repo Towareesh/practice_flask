36a9d75eb1c584da449edf781de19c5fece7dfb8
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-02-19 23:10:08 +0200

    fixbug

--- app/models.py
+++ app/models.py
@@ -76,7 +76,7 @@ class User(UserMixin, db.Model):
     def verify_reset_password_token(token):
         try:
             id = jwt.decode(token, app.config['SECRET_KEY'],
-                            algorithm=['HS256'])['reset_password']
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
