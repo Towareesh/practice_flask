4e25cb0d42373eb0316f73e5c420a4f29f8a00ce
Towareesh <63032856+Towareesh@users.noreply.github.com>
2023-02-23 22:05:51 +0200

    refactor
    Refactoring user posts view

--- app/views.py
+++ app/views.py
@@ -156,15 +156,17 @@ def logout():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    form = EmptyForm()
     user  = User.query.filter_by(username=username).first_or_404()
     page  = request.args.get('page', 1, type=int)
     
-    #### later add: per_page=app.config['POSTS_PER_PAGE']
-    posts = user.posts.paginate(page=page,
-                                error_out=False)
-    return render_template('user.html', user=user,
-                           form=form, posts=posts.items)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page=page, error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    
+    form = EmptyForm()
+
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
